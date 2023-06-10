e965b28af6f0a606a3f4a2b3a927b3b91e6c0878
Kosherbay Gassyrbek <gassyrbek2001@gmail.com>
2023-06-10 16:50:22 +0300

    feat: add

--- src/system/system.cpp
+++ src/system/system.cpp
@@ -48,14 +48,18 @@ void System::solve(double t) {
 }
 double System::l_1(double t) {
     double res = 0;
-    for (int i = J + 1; i < Mx - 1; ++i) {
-        res += abs(GetValue(i)(1) - GetExactSol(i, x0, omega,  t)(1));
+    // for (int i = J + 1; i < Mx - 1; ++i) {
+    //     res += abs(GetValue(i)(1) - GetExactSol(i, x0, omega,  t)(1));
+    // }
+    // return res / (Mx - J - 2);
+    for (int i = 0; i < Mx; ++i) {
+        res += abs(GetValue(i)(1) - GetExactSol(i, x0, omega,  t)(1)) * h;
     }
-    return res / (Mx - J - 2);
+    return res;
 }
 double System::l_inf(double t) {
     double res = 0.0;
-    for (int i = J + 1; i < Mx - 1; ++i) {
+    for (int i = 0; i < Mx; ++i) {
          double temp = abs(GetValue(i)(1) - GetExactSol(i, x0, omega, t)(1));
          if (temp >= res) res = temp;
     }
@@ -64,27 +68,27 @@ double System::l_inf(double t) {
 void System::sample() {
     int n = 0;
     while (n < total_steps) {
-        std::ostringstream filename;
-        std::ostringstream exact;
-        filename << "../bin/animation/velocity_out_" << std::setfill('0') << std::setw(5) << n << ".bin";
-        exact << "../bin/animation/exact_out_" << std::setfill('0') << std::setw(5) << n << ".bin";
-
-        std::ofstream file_velocity(filename.str(), std::ios::binary);
-        std::ofstream file_exact(exact.str(), std::ios::binary);
-        // Запись количества точек данных для текущего временного шага (4 байта, little-endian)
-        for (int i = 0; i < Mx; ++i) {
-            float x_coord = h * i;
-            float pressure_value = GetValue(i)(1);
-            float exact_pressure = GetExactSol(i, x0, omega, n * tau)(1);
-            // Запись x, y и p(x, y) (каждый параметр - 4 байта, little-endian)
-            file_velocity.write(reinterpret_cast<char*>(&x_coord), sizeof(x_coord));
-            file_velocity.write(reinterpret_cast<char*>(&pressure_value), sizeof(pressure_value));
-
-            file_exact.write(reinterpret_cast<char*>(&x_coord), sizeof(x_coord));
-            file_exact.write(reinterpret_cast<char*>(&exact_pressure), sizeof(exact_pressure));
-        }
-        file_velocity.close();
-        file_exact.close();
+        // std::ostringstream filename;
+        // std::ostringstream exact;
+        // filename << "../bin/animation/velocity_out_" << std::setfill('0') << std::setw(5) << n << ".bin";
+        // exact << "../bin/animation/exact_out_" << std::setfill('0') << std::setw(5) << n << ".bin";
+        //
+        // std::ofstream file_velocity(filename.str(), std::ios::binary);
+        // std::ofstream file_exact(exact.str(), std::ios::binary);
+        // // Запись количества точек данных для текущего временного шага (4 байта, little-endian)
+        // for (int i = 0; i < Mx; ++i) {
+        //     float x_coord = h * i;
+        //     float pressure_value = GetValue(i)(1);
+        //     float exact_pressure = GetExactSol(i, x0, omega, n * tau)(1);
+        //     // Запись x, y и p(x, y) (каждый параметр - 4 байта, little-endian)
+        //     file_velocity.write(reinterpret_cast<char*>(&x_coord), sizeof(x_coord));
+        //     file_velocity.write(reinterpret_cast<char*>(&pressure_value), sizeof(pressure_value));
+        //
+        //     file_exact.write(reinterpret_cast<char*>(&x_coord), sizeof(x_coord));
+        //     file_exact.write(reinterpret_cast<char*>(&exact_pressure), sizeof(exact_pressure));
+        // }
+        // file_velocity.close();
+        // file_exact.close();
         n++;
         solve(n * tau);
     }
@@ -94,8 +98,8 @@ void System::sample() {
 }
 System::System(double _tau, int _M, double _x0, double _A, double _omega)
 : PreProcess(_tau, _M, _x0, _A, _omega) {
-    // total_time = (alpha - _x0) / c_minus + 0.5 * (2 - alpha) / c_plus;
-    total_time = (alpha - _x0) / c_minus + 0.5 / c_plus;
+    total_time = (alpha - _x0) / c_minus + 0.5 * (2 - alpha - _omega * c_plus / c_minus) / c_plus;
+    // total_time = (alpha - _x0) / c_minus + 0.5 / c_plus;
     total_steps = total_time / tau;
     std::cout << "Total Steps: " << total_steps << "\nTotal Time: " << total_time << std::endl;
 }
